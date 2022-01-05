923d42f58174beafc7639cddfcebf967fccaeb18
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2022-01-05 18:09:28 +0900

    userのstateが変更できることを確認した

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import "./App.css";
 import { useAppDispatch, useAppSelector } from "./app/hooks";
-import { login, logout, selectUser } from "./features/user/userSlicer";
+import {
+  login,
+  logout,
+  selectUser,
+  updateUserState,
+} from "./features/user/userSlicer";
+import { UserStateType } from "./types/userStateType";
 
 function App() {
   const user = useAppSelector(selectUser);
@@ -24,6 +30,12 @@ function App() {
     );
   };
 
+  const onUpdateState = (state: UserStateType) => {
+    if (user.uid) {
+      dispatch(updateUserState(state));
+    }
+  };
+
   const onLogout = () => {
     dispatch(logout(undefined));
   };
@@ -35,6 +47,14 @@ function App() {
       <button onClick={() => onLogin(false)}>LOGIN</button>
       <br />
       <button onClick={() => onLogin(true)}>LOGIN as Admin</button>
+      <br />
+      <button onClick={() => onUpdateState({ state: "free" })}>
+        State change Free
+      </button>
+      <br />
+      <button onClick={() => onUpdateState({ state: "busy" })}>
+        State change Busy
+      </button>
       <br />
       <button onClick={onLogout}>LOGOUT</button>
     </>
