3f3aedfb7d015d8d71389ee4b747c4c439c20056
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2022-01-07 13:22:26 +0900

    userTableをとりあえず表示させた

--- src/components/pages/AdminUserPage.tsx
+++ src/components/pages/AdminUserPage.tsx
@@ -1,9 +1,93 @@
-import React from "react";
-import MyHeader from "../organisms/Header";
+import React, { useEffect, useState } from "react";
 
-const AdminUserPage = () => {
+import MaterialTable from "material-table";
+import { UserStateType } from "../../types/userStateType";
+import { stateUserType } from "../../features/user/userSlicer";
+
+const inputUserData: Array<stateUserType> = [
+  {
+    uid: "1",
+    photoUrl:
+      "https://firebasestorage.googleapis.com/v0/b/twitter-cloneapp-2c188.appspot.com/o/avatar%2F0.3bhndevu9y2_avatar1.png?alt=media&token=42547298-4ca8-417c-aa9f-509f916160e0",
+    displayName: "userName",
+    state: { state: "free" },
+    isAdmin: false,
+    currentTask: "None",
+  },
+  {
+    uid: "1",
+    photoUrl:
+      "https://firebasestorage.googleapis.com/v0/b/twitter-cloneapp-2c188.appspot.com/o/avatar%2F0.3bhndevu9y2_avatar1.png?alt=media&token=42547298-4ca8-417c-aa9f-509f916160e0",
+    displayName: "userName",
+    state: { state: "free" },
+    isAdmin: false,
+    currentTask: "None",
+  },
+  {
+    uid: "2",
+    photoUrl:
+      "https://firebasestorage.googleapis.com/v0/b/twitter-cloneapp-2c188.appspot.com/o/avatar%2F0.3bhndevu9y2_avatar1.png?alt=media&token=42547298-4ca8-417c-aa9f-509f916160e0",
+    displayName: "userName",
+    state: { state: "free" },
+    isAdmin: false,
+    currentTask: "None",
+  },
+  {
+    uid: "2",
+    photoUrl:
+      "https://firebasestorage.googleapis.com/v0/b/twitter-cloneapp-2c188.appspot.com/o/avatar%2F0.3bhndevu9y2_avatar1.png?alt=media&token=42547298-4ca8-417c-aa9f-509f916160e0",
+    displayName: "userName",
+    state: { state: "free" },
+    isAdmin: false,
+    currentTask: "None",
+  },
+];
+
+// 適宜UserStateTypeが追加されてTableが追加したい時は追加
+type TableUserType = {
+  //info: Pick<stateUserType, "uid" | "displayName" | `currentTask`>;
+  uid: string;
+  displayName: string;
+  currentTask: string;
+  state: "free" | "busy";
+};
+
+const tableColumns = [
+  { title: "ID", field: "uid" },
+  { tilte: "Name", field: "displayName" },
+  { title: "Task", field: "currentTask" },
+  { title: "State", field: "state" },
+];
+
+const AdminUserPage: React.VFC = () => {
+  const [showData, setShowData] = useState<Array<TableUserType>>([
+    {
+      uid: "a",
+      displayName: "username",
+      currentTask: "cleanRoom",
+      state: "free",
+    },
+  ]);
+
+  // useEffect(() => {
+  //   // 本当はFirestoreから取ってくる
+  //   setShowData(
+  //     inputUserData.map((usr) => ({
+  //       uid: usr.uid,
+  //       displayName: usr.displayName,
+  //       currentTask: usr.currentTask,
+  //       state: usr.state,
+  //     }))
+  //   );
+  // }, []);
+
   return (
     <>
+      <MaterialTable
+        columns={tableColumns}
+        data={showData}
+        options={{ showTitle: false }}
+      />
       <div>AdminUserPage</div>
     </>
   );
