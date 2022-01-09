d908e1b96527a1cd0023f8778d2a86619869e3a7
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2022-01-09 20:19:23 +0900

    Logoutできるようにした

--- src/Hooks/useLogin.ts
+++ src/Hooks/useLogin.ts
@@ -1,7 +1,9 @@
+import { signOut } from "firebase/auth";
 import React, { useCallback } from "react";
 import { useHistory } from "react-router-dom";
 import { useAppDispatch } from "../app/hooks";
 import { login, logout } from "../features/user/userSlicer";
+import { auth } from "../firebase/firebase";
 import { DBUserType } from "../types/userStateType";
 
 const useLogin = () => {
@@ -25,8 +27,19 @@ const useLogin = () => {
   // Logout
   const onLogout = () => {
     //firebaseのLogout処理
+    const onSignOut = async () => {
+      console.log("try:signOut");
+      try {
+        await signOut(auth);
+        dispatch(logout(undefined));
+        console.log("success:signOut");
+      } catch (e: any) {
+        alert(e.message);
+      }
+    };
 
-    dispatch(logout(undefined));
+    onSignOut();
+
     // urlの /移動
     history.push("/");
   };
