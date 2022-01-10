cd850c49d6b74abb25ebf45b3b3050c5aabffbb9
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2022-01-10 09:02:50 +0900

    Freeページを作った

--- src/components/pages/GuestPages/GuestFreePaga.tsx
+++ src/components/pages/GuestPages/GuestFreePaga.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useAppSelector } from "../../../app/hooks";
+import { selectUser } from "../../../features/user/userSlicer";
 
 const GuestFreePaga = () => {
-  return <div>GuestFreePage</div>;
+  const user = useAppSelector(selectUser);
+
+  return (
+    <>
+      <h2>{user.info.displayName}さんは今、</h2>
+      <h1>休憩中</h1>
+      <h2>です</h2>
+    </>
+  );
 };
 
 export default GuestFreePaga;
