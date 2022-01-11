6f3d813f3c05cf92eee90658c9375b3e8940c669
YuukiHnf <hnf_yuuki@eis.hokudai.ac.jp>
2022-01-11 13:29:20 +0900

    おやぶんって名前にしたのとbotのImageを入れた

--- src/components/pages/GuestPage.tsx
+++ src/components/pages/GuestPage.tsx
@@ -35,6 +35,10 @@ const GuestPage = () => {
   return (
     <>
       <div>GUEST USER : {user.info.displayName}さん</div>
+      <img
+        src="https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG29mKfDPUdcP3LGnVI6yE5FijAJDq9h2DXur7w3cZMFbaf3wbpF0Q3rIdTPVuRC-R_yKydiFfhnn2zaKsl29Z01rrPAVeYexiF5n7XDAxuOT3z6Ls1VWliBMDXKO8cr6Z1wg8FezC-TNk_qdv-dUWZLqJP_bQTU9cHyL7g-lJkbrKRBng-qcCkLU9S1fw0DqNa_76aSm2vIjFfKjoFJcusRuM3h2JlcxZXInyrTqixUZNrtCGJclQI67vI3EqkhKnVFi6jxHwKdG0AuupvEJCfY=/bot-icon-robot-logotype-flat-260nw-1497911912.jpg?errorImage=false"
+        style={{ width: "30%", height: "auto" }}
+      />
 
       {user.userTaskState.state === "busy" ? (
         <GuestBusyPage />
